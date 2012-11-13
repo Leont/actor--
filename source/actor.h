196722f7c8c1c54140635cf6fb6431b22a47f325
Leon Timmermans <fawaka@gmail.com>
2012-11-13 22:39:31 +0100

    Don't keep alive queues in actors

--- source/actor.h
+++ source/actor.h
@@ -2,7 +2,7 @@
 #include <mutex>
 #include <condition_variable>
 #include <queue>
-#include <utility>
+#include <memory>
 
 namespace actor {
 	template<typename T> class queue {
@@ -13,12 +13,12 @@ namespace actor {
 		queue<T>& operator=(const queue<T>&) = delete;
 		public:
 		queue() : mutex(), cond(), messages() { };
-		void push(T&& value) noexcept {
+		void push(T&& value) {
 			std::lock_guard<std::mutex> lock(mutex);
 			messages.push(value);
 			cond.notify_one();
 		}
-		T pop() noexcept {
+		T pop() {
 			std::unique_lock<std::mutex> lock(mutex);
 			while (messages.empty());
 				cond.wait(lock);
@@ -28,22 +28,29 @@ namespace actor {
 
 	template<typename T> class receiver {
 		const std::shared_ptr<queue<T>> mailbox;
-		public:
+		template<typename> friend class actor;
 		receiver(const std::shared_ptr<queue<T>>& _mailbox) : mailbox(_mailbox) { }
-		T receive() noexcept {
+		public:
+		T receive() const {
 			return mailbox->pop();
 		}
 	};
 
 	template<typename T> class actor {
-		std::shared_ptr<queue<T>> mailbox;
+		std::weak_ptr<queue<T>> mailbox_ref;
 		public:
-		template<typename U, typename... V> actor(U&& u, V&&... params) noexcept : mailbox(new queue<T>()) {
-			std::thread thr(u, receiver<T>(mailbox), params...);
-			thr.detach();
+		template<typename U, typename... V> actor(U&& u, V&&... _params) : mailbox_ref([&](V&&... params){
+			auto mailbox = std::make_shared<queue<T>>();
+			std::thread(u, receiver<T>(mailbox), std::forward<V>(params)...).detach();
+			return mailbox;
+		}(std::forward<V>(_params)...)) {}
+		void send(T&& value) const {
+			auto mailbox = mailbox_ref.lock();
+			if (mailbox)
+				mailbox->push(value);
 		}
-		void send(T&& value) noexcept {
-			mailbox->push(value);
+		bool alive() const noexcept {
+			return !mailbox_ref.expired();
 		}
 	};
 }
