6b3853447a3705b7d338a74c55ae4aa6aec22efd
Leon Timmermans <fawaka@gmail.com>
2014-03-30 19:19:20 +0300

    Added chameneos example

---
+++ chameneosredux.C
@@ -0,0 +1,148 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <future>
+#include "source/actor.h"
+
+std::string spell(int n) {
+	static std::string numbers[] = {
+	   " zero", " one", " two",
+	   " three", " four", " five",
+	   " six", " seven", " eight",
+	   " nine"
+	};
+
+	std::string str;
+	do {
+	   str.insert( 0, numbers[n % 10] );
+	   n /= 10;
+	} while (n);
+
+	return str;
+}
+
+enum color { blue = 0, red, yellow };
+
+// stream operator to write a color
+
+std::ostream& operator<<(std::ostream &s, const color &c ) {
+   static const char *names[] = { "blue", "red", "yellow" };
+   s << names[c];
+   return s;
+}
+
+color operator+(const color &c1, const color &c2) {
+   switch ( c1 ) {
+      case blue: switch ( c2 ) {
+         case blue:   return blue;
+         case red:    return yellow;
+         case yellow: return red;
+         default:;
+      }
+      case red: switch ( c2 ) {
+         case blue:   return yellow;
+         case red:    return red;
+         case yellow: return blue;
+         default:;
+      }
+      case yellow: switch ( c2 ) {
+         case blue:   return red;
+         case red:    return blue;
+         case yellow: return yellow;
+         default:;
+      }
+      default:;
+   }
+   throw "Invalid";
+}
+
+static void show_complements() {
+	for (auto i : { blue, red, yellow }) {
+		for (auto j : { blue, red, yellow }) {
+			std::cout << i << " + " << j << " = " << (i + j) << std::endl;
+		}
+	}
+}
+
+static std::mutex output_mutex;
+
+void print_header(std::initializer_list<color> colors) {
+	std::lock_guard<std::mutex> lock(output_mutex);
+	std::cout << std::endl;
+	for (auto i : colors)
+		std::cout << " " << i;
+	std::cout << std::endl;
+}
+
+struct message {
+	const actor::actor<message>* chameneos;
+	const color colour;
+	message(const actor::actor<message>* _chameneos, color  _colour) : chameneos(_chameneos), colour(_colour) {}
+};
+
+static void broker_func(const actor::actor<message>&, const actor::receiver<message> recv, size_t meetings_count, size_t color_count) {
+	for (auto i = 0u; i < meetings_count; ++i) {
+		message left = recv.receive();
+		message right = recv.receive();
+		left.chameneos->send(right);
+		right.chameneos->send(left);
+	}
+	for (auto i = 0u; i < color_count; ++i) {
+		message last = recv.receive();
+		last.chameneos->kill();
+	}
+}
+
+static void cleanup_func(const actor::actor<size_t>&, const actor::receiver<size_t> recv, size_t color_count, actor::queue<size_t>& result) {
+	size_t summary = 0;
+	for (auto i = 0u; i < color_count; ++i) {
+		summary += recv.receive();
+	}
+	std::lock_guard<std::mutex> lock(output_mutex);
+	std::cout << spell(summary) << std::endl;
+	result.push(summary);
+}
+
+
+static void chameneos_func(const actor::actor<message>& self, const actor::receiver<message>& recv, color start_color, const actor::actor<message>& broker, const actor::actor<size_t> cleanup) {
+	size_t meetings = 0, met_self = 0;
+	color current = start_color;
+	try {
+		while (1) {
+			broker.send(message(&self, current));
+			message tmp = recv.receive();
+			meetings++;
+			current = current + tmp.colour;
+			if (tmp.chameneos == &self)
+				met_self++;
+		}
+	}
+	catch (const actor::death&) {
+		std::lock_guard<std::mutex> lock(output_mutex);
+		std::cout << meetings << " " << met_self << std::endl;
+		cleanup.send(meetings);
+	}
+}
+
+static void run(std::initializer_list<color> colors, size_t count) {
+	print_header(colors);
+	actor::actor<message> broker(broker_func, count, colors.size());
+	actor::queue<size_t> result;
+	actor::actor<size_t> cleanup(cleanup_func, colors.size(), std::ref(result));
+	std::vector<actor::actor<message>> chameneoses;
+	for (auto color : colors) {
+		chameneoses.emplace_back(chameneos_func, color, broker, cleanup);
+	}
+	result.pop();
+	return;
+}
+
+int main (int argc, char ** argv) {
+	std::vector<std::string> args(argv + 1, argv + argc);
+	size_t count = args.size() ? std::stoul(args.at(0)) : 10;
+	show_complements();
+	run({ blue, red, yellow }, count);
+	run({ blue, red, yellow, red, yellow, blue, red, yellow, red, blue }, count);
+	std::cout << std::endl;
+	return 0;
+}
